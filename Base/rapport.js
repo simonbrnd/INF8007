a0105f02422c8abe828af23f8323b72de7a4bf26
simonbrnd <simon.bernard1@eleves.enpc.fr>
2022-02-15 22:28:35 -0500

    Add surligner

--- Base/rapport.js
+++ Base/rapport.js
@@ -23,8 +23,41 @@ let calculerStatistiques = () => {
     return {"min" : min, "max" : max, "medianes" : meds}
 }
 
-let surligner = () => {
-    
+let surligner = (stats) => {
+
+    let tr_rows = document.getElementsByName("dataframe-row")
+    let td_nb_inters = document.getElementsByName("nb-interventions")
+    let td_emplacements = document.getElementsByName("emplacement")
+    for(i in td_nb_inters){
+        let tr_row = tr_rows[i]
+        let td_nb_inter = td_nb_inters[i]
+        let td_emplacement = td_emplacements[i]
+        let nbInterventions = td_nb_inter.textContent
+        let tr_class = ""
+        
+        if (nbInterventions == stats.min) {
+            tr_class = "bg-success text-light font-weight-bold"
+            let badge = document.createElement("div")
+            badge.setAttribute("class", "badge badge-light text-wrap")
+            badge.textContent = "région la moins touchée"
+            td_emplacement.appendChild(badge)
+          } else if (nbInterventions == stats.max) {
+            tr_class = "bg-danger text-light font-weight-bold" 
+            let badge = document.createElement("div")
+            badge.setAttribute("class", "badge badge-light text-wrap")
+            badge.textContent = "région la plus touchée"
+            td_emplacement.appendChild(badge)
+          } else if (stats.medianes.includes(parseInt(nbInterventions))) {
+            tr_class = "bg-warning font-weight-bold"
+            let badge = document.createElement("div")
+            badge.setAttribute("class", "badge badge-light text-wrap")
+            badge.textContent = "région moyennement touchée"
+            td_emplacement.appendChild(badge)
+          } 
+
+        tr_row.setAttribute("class", tr_class)
+    } 
+
 }
 
 
@@ -32,41 +65,29 @@ let remplirTableau = () => {
     let tbody = document.getElementById("interventions-par-poste")
     let total_cases = 0
 
-    let stats = calculerStatistiques()
-    console.log(stats)
+    let statistiques = calculerStatistiques()
     
     for(element of data.nbInterventionsParPDQ){
         let tr = document.createElement("tr")
-        let tr_class = ""
-
-        let nbInterventions = element.nbInterventions
-
-        if (nbInterventions == stats.min) {
-            tr_class = "bg-success text-light font-weight-bold"
-          } else if (nbInterventions == stats.max) {
-            tr_class = "bg-danger text-light font-weight-bold" 
-          } else if (stats.medianes.includes(nbInterventions)) {
-            tr_class = "bg-warning font-weight-bold"
-          } 
-
-        tr.className = tr_class
-        
+        tr.setAttribute("name", "dataframe-row")
         tbody.appendChild(tr)
 
         let td_PDQ = document.createElement("td")
+        td_PDQ.setAttribute("name", "pqd")
         td_PDQ.textContent = element.PDQ
         tr.appendChild(td_PDQ)
 
         let td_Emplacement = document.createElement("td")
-        td_Emplacement.textContent = element.Emplacement
+        td_Emplacement.setAttribute("name", "emplacement")
+        td_Emplacement.textContent = element.Emplacement + " "
         tr.appendChild(td_Emplacement)
 
         let td_nbInterventions = document.createElement("td")
-        td_nbInterventions.textContent = nbInterventions
+        td_nbInterventions.textContent = element.nbInterventions
+        td_nbInterventions.setAttribute("name", "nb-interventions")
         td_nbInterventions.className = "text-right"
         total_cases += element.nbInterventions
         tr.appendChild(td_nbInterventions)
-
     }
 
     let tr = document.createElement("tr")
@@ -86,6 +107,8 @@ let remplirTableau = () => {
     th_nbInterventions.textContent = total_cases
     th_nbInterventions.className = "text-right"
     tr.appendChild(th_nbInterventions)
+
+    setTimeout(surligner(statistiques),1000)
 
 }
 remplirTableau()
